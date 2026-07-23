af940b9c1e6d2a1fa8254dc127dc424c59a2e78f
adityapratap1409 <aditya.sjc@gmail.com>
2026-07-23 10:52:11 +0530

    Add comment to clarify swapping functionality in swapper function

--- Arrays/swapalternate.cpp
+++ Arrays/swapalternate.cpp
@@ -3,7 +3,7 @@ using namespace std;
 void swapper(int arr[],int size){
     for(int i=0;i<size;i+=2){
         if(i+1<size){
-            swap(arr[i],arr[i+1]);
+            swap(arr[i],arr[i+1]);// does swapping
         }
     }
 }
